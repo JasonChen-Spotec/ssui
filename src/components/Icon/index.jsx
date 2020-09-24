3d7596a20809501b4f67b628362f88d54fe5ef35
jason <usochen@gmail.com>
2020-09-24 16:38:54 +0800

    feat: add icon components

---
+++ src/components/Icon/index.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { string, oneOf, bool } from 'prop-types';
+import classNames from 'classnames';
+
+const IconPropTypes = {
+
+  symbolPrefix: string,
+  name: string.isRequired,
+  size: oneOf(['sm', 'md', 'lg']),
+  type: oneOf(['normal', 'link', 'error', 'warning', 'info', 'success']),
+  focusable: bool
+};
+
+const IconDefaultProps = {
+  symbolPrefix: 'spotec-icon',
+  size: 'md',
+  type: 'normal',
+  focusable: false
+};
+
+
+const Icon = props => {
+  const { className, name, size, type, symbolPrefix, ...rest } = props;
+  const cls = classNames(
+    'icon-svg',
+    `icon-svg-${name}`,
+    `icon-svg-size-${size}`,
+    `icon-svg-type-${type}`,
+    className
+  );
+
+  return (
+    <svg
+      className={cls}
+      {...rest}
+    >
+      <use xlinkHref={`#${[symbolPrefix, name].join('-')}`} />
+    </svg>
+  );
+};
+
+Icon.propTypes = IconPropTypes;
+Icon.defaultProps = IconDefaultProps;
+
+export default Icon;
