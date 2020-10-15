dcbaac8b2209cc7e9e6806d61919be15e7e9e813
jaon <>
2020-10-15 13:55:54 +0800

    feat: add pendingFilled icon

---
+++ src/icons/PendingFilled.js
@@ -0,0 +1,17 @@
+import * as React from "react";
+import Icon from "../components/Icon";
+
+function PendingFilled(componentProps) {
+  const IconNode = (props) => (
+    <svg viewBox="0 0 16 16" {...props}>
+      <path
+        fill="currentColor"
+        d="M8 0a8 8 0 108 8 8 8 0 00-8-8zM3.79 9.08A1.08 1.08 0 114.87 8a1.08 1.08 0 01-1.08 1.08zm4.14 0A1.08 1.08 0 119 8a1.08 1.08 0 01-1.07 1.08zm4.15 0A1.08 1.08 0 1113.16 8a1.08 1.08 0 01-1.08 1.08z"
+      />
+    </svg>
+  );
+
+  return <Icon {...componentProps} component={IconNode} />;
+}
+
+export default PendingFilled;
--- src/svgs/demo/base.jsx
+++ src/svgs/demo/base.jsx
@@ -1,28 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
+import { InputNumber } from 'antd';
+import { SketchPicker } from 'react-color';
 import { message } from 'antd';
 import iconsList from './iconsList';
 import './index.less';
 
 export default () => {
+  const [ color, setColor ] = useState('#000000')
+  const [ width, setWidth ] = useState(24)
   return (
-    <div className="icons-container">
-      {
-        iconsList.map((Icon, index)=>(
-          <CopyToClipboard
-            key={index}
-            text={`<${Icon.name} />`}
-            onCopy={() => message.info(`copy <${Icon.name} /> success`)}
-          >
-            <div className="icons-item">
-              <Icon style={{ width: 24 }} />
-              <span>
-                {Icon.name}
-              </span>
-            </div>
-          </CopyToClipboard>
-        ))
-      }
+    <div>
+      <div className="icon-params">
+        <SketchPicker
+          className="color-input"
+          onChange={(value)=> setColor(value.hex)}
+          color={color}
+        />
+        <div>
+          <span className="label">宽度</span>
+          <InputNumber value={width} onChange={(value)=> setWidth(value)} />
+        </div>
+      </div>
+      <div className="icons-container">
+        {
+          iconsList.map((Icon, index)=>(
+            <CopyToClipboard
+              key={index}
+              text={`<${Icon.name} />`}
+              onCopy={() => message.info(`copy <${Icon.name} /> success`)}
+            >
+              <div className="icons-item">
+                <Icon style={{ width, color, fill: 'red'}} />
+                <span>
+                  {Icon.name}
+                </span>
+              </div>
+            </CopyToClipboard>
+          ))
+        }
+      </div>
     </div>
   );
 };
