4d553e33016c4dab6bf7b20cc6cecee9a99495fd
jaon <>
2020-11-04 16:06:54 +0800

    chore: release @vant/icons 1.0.33

--- src/.umi/core/plugin.ts
+++ src/.umi/core/plugin.ts
@@ -1,5 +1,5 @@
 // @ts-nocheck
-import { Plugin } from '/Users/morganqin/Documents/GitHub/assui/node_modules/@umijs/runtime';
+import { Plugin } from '/Users/jianchen/work/assui/node_modules/@umijs/runtime';
 
 const plugin = new Plugin({
   validKeys: ['modifyClientRenderOpts','patchRoutes','rootContainer','render','onRouteChange',],
