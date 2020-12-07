95e873fa75efc512178863280c8294928231f1d3
MorganQin-Spotec <morganqin-spotec@outlook.com>
2020-12-07 18:02:13 +0800

    feat: ITM-747 add icons for itm capital list

---
+++ src/icons/EllipsisOutlined.js
@@ -0,0 +1,16 @@
+import * as React from "react";
+import Icon from "assui/lib/components/Icon";
+
+function EllipsisOutlined(componentProps) {
+  const IconNode = (props) => (
+    <svg viewBox="0 0 16 16" {...props}>
+      <circle fill="currentColor" cx={1.06} cy={8} r={1.06} />
+      <circle fill="currentColor" cx={8} cy={8} r={1.06} />
+      <circle fill="currentColor" cx={14.94} cy={8} r={1.06} />
+    </svg>
+  );
+
+  return <Icon {...componentProps} component={IconNode} />;
+}
+
+export default EllipsisOutlined;
