92910231bffd78bc825cc2321f4a02323198a16a
jaon <>
2020-12-21 14:22:46 +0800

    fix: number-input text error

--- src/components/number-input/demo/index.jsx
+++ src/components/number-input/demo/index.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Form, Button } from 'antd';
-// import NumberInput from 'assui/lib/components/number-input';
-import NumberInput from '../'
+import NumberInput from 'assui/lib/components/number-input';
 
 const Demo = () => {
   const onFinish = values => {
@@ -9,48 +8,44 @@ const Demo = () => {
   };
 
   return (
-    <>
-      <NumberInput />
-
-      <Form
-        onFinish={onFinish}
-        initialValues={
-          {
-            int: '2323'
-          }
+    <Form
+      onFinish={onFinish}
+      initialValues={
+        {
+          int: '2323'
         }
+      }
+    >
+      <Form.Item
+        name="int"
+        label="int"
       >
-        <Form.Item
-          name="int"
-          label="int"
-        >
-          <NumberInput />
-        </Form.Item>
-        <Form.Item
-          name="float"
-          label="float"
-        >
-          <NumberInput numberType="float" />
-        </Form.Item>
-        <Form.Item
-          name="intEnableMinus"
-          label="int(enableMinus)"
-        >
-          <NumberInput enableMinus />
-        </Form.Item>
-        <Form.Item
-          name="floatEnableMinus"
-          label="float(enableMinus)"
-        >
-          <NumberInput numberType="float" enableMinus />
-        </Form.Item>
-        <Form.Item>
-          <Button type="primary" htmlType="submit">
-          Submit
-          </Button>
-        </Form.Item>
-      </Form>
-    </>
+        <NumberInput />
+      </Form.Item>
+      <Form.Item
+        name="float"
+        label="float"
+      >
+        <NumberInput numberType="float" />
+      </Form.Item>
+      <Form.Item
+        name="intEnableMinus"
+        label="int(enableMinus)"
+      >
+        <NumberInput enableMinus />
+      </Form.Item>
+      <Form.Item
+        name="floatEnableMinus"
+        label="float(enableMinus)"
+      >
+        <NumberInput numberType="float" enableMinus />
+      </Form.Item>
+      <Form.Item>
+        <Button type="primary" htmlType="submit">
+        Submit
+        </Button>
+      </Form.Item>
+    </Form>
   );
 };
 
