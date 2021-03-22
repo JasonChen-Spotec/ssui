9b606e6d34ba05bba28d37052a588a9451d3853c
jaon <>
2021-03-22 14:48:21 +0800

    feat: add svg icon

---
+++ src/icons/BOutlined.js
@@ -0,0 +1,18 @@
+import * as React from "react";
+import Icon from "assui/lib/components/Icon";
+
+function BOutlined(componentProps) {
+  const IconNode = (props) => (
+    <svg viewBox="0 0 16 16" {...props}>
+      <path
+        fill="currentColor"
+        d="M5.94.56v1.23h-1.5a1 1 0 00-1 1v10.36a1 1 0 001 .95h1.5v1.24a.61.61 0 00.61.61.61.61 0 00.62-.61V14.1h1.24v1.24A.61.61 0 009 16a.61.61 0 00.61-.61V14.1h.86a3.39 3.39 0 001.68-6.36A3.39 3.39 0 009.94 1.8h-.3V.56A.61.61 0 009 0a.6.6 0 00-.61.61v1.18H7.18V.56A.61.61 0 006.56 0a.61.61 0 00-.62.56zM5.17 3h4.72a2.18 2.18 0 012.2 2.1 2.15 2.15 0 01-2.16 2.24H4.71V3.47A.46.46 0 015.17 3zm-.46 5.57h5.81a2.17 2.17 0 012.19 2.1 2.14 2.14 0 01-2.16 2.21H5.17a.45.45 0 01-.46-.45z"
+      />
+    </svg>
+  );
+
+  return <Icon {...componentProps} component={IconNode} />;
+}
+
+BOutlined.displayName = "BOutlined";
+export default BOutlined;
---
+++ src/icons/CloudOutlined.js
@@ -0,0 +1,18 @@
+import * as React from "react";
+import Icon from "assui/lib/components/Icon";
+
+function CloudOutlined(componentProps) {
+  const IconNode = (props) => (
+    <svg viewBox="0 0 16 16" {...props}>
+      <path
+        fill="currentColor"
+        d="M12 13.78H3.79A3.6 3.6 0 010 10.41a3.57 3.57 0 013.46-3.69 4.55 4.55 0 014.31-4.49 4.58 4.58 0 014.88 3.9 3.86 3.86 0 013.35 4.1 3.92 3.92 0 01-4 3.55zm-7.73-1.29H12a2.65 2.65 0 002.28-1.28 2.5 2.5 0 000-2.56A2.64 2.64 0 0012 7.36a.69.69 0 01-.47-.18.66.66 0 01-.19-.46 3.24 3.24 0 00-3.29-3.2 3.25 3.25 0 00-3.3 3.2v.14a3.55 3.55 0 012.64 3.39.63.63 0 01-.33.55.65.65 0 01-.66 0 .63.63 0 01-.33-.55A2.28 2.28 0 003.8 8a2.25 2.25 0 100 4.49h.47z"
+      />
+    </svg>
+  );
+
+  return <Icon {...componentProps} component={IconNode} />;
+}
+
+CloudOutlined.displayName = "CloudOutlined";
+export default CloudOutlined;
