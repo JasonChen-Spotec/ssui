24502382fe18742dd8b8fb1a8ea1289e8232b2c8
JasonChen-Spotec <46470667+JasonChen-Spotec@users.noreply.github.com>
2021-11-16 19:03:16 +0800

    feat: modify condition props type

--- packages/assui/src/condition-input/index.tsx
+++ packages/assui/src/condition-input/index.tsx
@@ -6,7 +6,7 @@ import parse2RegexOption from './parse2RegexOption';
 
 export interface ConditionInputProps {
   /** 过滤的正则条件 */
-  regexp: RegExp | (RegExp | string | { pattern: RegExp; replacement?: string })[];
+  regexp?: RegExp | (RegExp | string | { pattern: RegExp; replacement?: string })[];
   /** 指定输入框展示值的格式 */
   formatter?: (value: string) => string;
   /** 变化回调 */
@@ -22,15 +22,17 @@ const ConditionInput = (props: ConditionInputProps) => {
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { value: originalValue } = e.target;
-
-    const resultRegexp = isArray(regexp) ? regexp : [regexp];
-    let nextValue = resultRegexp.reduce((cur: string, acc) => {
-      const regexOption = parse2RegexOption(acc);
-      if (regexOption.isValid) {
-        return cur.replace(regexOption.pattern, regexOption.replacement);
-      }
-      return cur;
-    }, originalValue) as string;
+    let nextValue = originalValue;
+    if (regexp) {
+      const resultRegexp = isArray(regexp) ? regexp : [regexp];
+      nextValue = resultRegexp.reduce((cur: string, acc) => {
+        const regexOption = parse2RegexOption(acc);
+        if (regexOption.isValid) {
+          return cur.replace(regexOption.pattern, regexOption.replacement);
+        }
+        return cur;
+      }, originalValue) as string;
+    }
 
     if (isFunction(formatter)) {
       nextValue = formatter(nextValue);
