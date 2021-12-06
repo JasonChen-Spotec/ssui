23555676ec34ff36ba597f41e9a29629ff8a53f9
JasonChen-Spotec <46470667+JasonChen-Spotec@users.noreply.github.com>
2021-12-06 16:13:14 +0800

    fix: ChevronUpFilled icon bug

--- packages/a-icons/src/ChevronUpFilled.tsx
+++ packages/a-icons/src/ChevronUpFilled.tsx
@@ -3,7 +3,7 @@ import Icon, { IconProps } from 'a-base-icon/lib/Icon';
 
 function ChevronUpFilled(componentProps: IconProps) {
   const IconNode = (props: React.SVGProps<SVGSVGElement>) => (
-    <svg width={24} height={24} fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+    <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
       <rect
         fill="currentColor"
         width={19.12}
