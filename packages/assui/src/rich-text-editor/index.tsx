d0f7580524822920c2c00b4b93798173d64861e8
JasonChen-Spotec <46470667+JasonChen-Spotec@users.noreply.github.com>
2021-12-16 10:29:10 +0800

    d

--- packages/assui/src/rich-text-editor/index.tsx
+++ packages/assui/src/rich-text-editor/index.tsx
@@ -1,6 +1,9 @@
+// @ts-nocheck
 import React from 'react';
 import type { IAllProps } from '@tinymce/tinymce-react';
 import { Editor } from '@tinymce/tinymce-react';
+import { Button } from 'antd';
+import type * as Type from 'tinymce/tinymce';
 import 'tinymce/tinymce';
 import 'tinymce/themes/silver';
 import 'tinymce/icons/default';
@@ -28,31 +31,77 @@ import 'tinymce/plugins/template';
 import 'tinymce/plugins/help';
 import './lang/zh_CN';
 
-const defaultInit = {
-  height: 300,
-  language: 'zh_CN',
-  menubar: false,
-  images_upload_handler() {},
-  plugins: [
-    ' autolink lists link image charmap  anchor',
-    'searchreplace code fullscreen',
-    'insertdatetime media table help wordcount',
-  ],
-  toolbar:
-    'undo redo | formatselect | ' +
-    'code bold italic color fontcolor backcolor | alignleft aligncenter ' +
-    'alignright alignjustify table | image bullist numlist outdent indent | ' +
-    'removeformat | help',
-  content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }',
-};
-
 export type RichTextEditorProps = IAllProps;
 
 const RichTextEditor = (props: IAllProps) => {
   const { init, ...restProps } = props;
+  const editRef = React.useRef();
+  const defaultInit = {
+    height: 300,
+    language: 'zh_CN',
+    menubar: false,
+    images_upload_handler() {},
+    plugins: [
+      ' autolink lists link image charmap  anchor',
+      'searchreplace code fullscreen',
+      'insertdatetime media table help wordcount',
+    ],
+    toolbar:
+      'undo redo | formatselect | ' +
+      'code bold italic color fontcolor backcolor | alignleft aligncenter ' +
+      'alignright alignjustify table | image bullist numlist outdent indent | ' +
+      'removeformat | help',
+    content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }',
+    setup(editor: Type.Editor) {
+      editRef.current = editor;
+
+      editor.ui.registry.addButton('customInsertButton', {
+        text: 'My Button',
+        onAction(_) {
+          editor.insertContent("&nbsp;<strong>It's my button!</strong>&nbsp;");
+        },
+      });
+
+      const toTimeHtml = function (date) {
+        return `<time datetime="${date.toString()}">${date.toDateString()}</time>`;
+      };
+
+      editor.ui.registry.addButton('customDateButton', {
+        icon: 'insert-time',
+        tooltip: 'Insert Current Date',
+        disabled: true,
+        onAction(_) {
+          editor.insertContent(toTimeHtml(new Date()));
+        },
+        onSetup(buttonApi) {
+          const editorEventCallback = function (eventApi) {
+            buttonApi.setDisabled(eventApi.element.nodeName.toLowerCase() === 'time');
+          };
+          editor.on('NodeChange', editorEventCallback);
+
+          /* onSetup should always return the unbind handlers */
+          return function (buttonApi) {
+            editor.off('NodeChange', editorEventCallback);
+          };
+        },
+      });
+    },
+  };
+
   const resultInit = { ...defaultInit, ...init };
 
-  return <Editor {...restProps} init={resultInit} />;
+  return (
+    <div className="test">
+      <Editor {...restProps} init={resultInit} />;
+      <Button
+        onClick={() => {
+          editRef.current.insertContent('<span>{{这是插入的tag}}</span>');
+        }}
+      >
+        测试
+      </Button>
+    </div>
+  );
 };
 
 export default RichTextEditor;
