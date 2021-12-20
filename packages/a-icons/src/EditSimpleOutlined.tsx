faf593102683663d86b574da542bbd57b95117a0
Ian <tester@testerdeMacBook-Pro.local>
2021-12-20 14:48:11 +0800

    feat:add-edit-simple-icon

--- packages/a-icons/src/EditSimpleOutlined.tsx
+++ packages/a-icons/src/EditSimpleOutlined.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import Icon, { IconProps } from 'a-base-icon/lib/Icon';
 
-function EditHoverOutlined(componentProps: IconProps) {
+function EditSimpleOutlined(componentProps: IconProps) {
   const IconNode = (props: React.SVGProps<SVGSVGElement>) => (
     <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
       <path
@@ -16,5 +16,5 @@ function EditHoverOutlined(componentProps: IconProps) {
   return <Icon {...componentProps} component={IconNode} />;
 }
 
-EditHoverOutlined.displayName = 'EditHoverOutlined';
-export default EditHoverOutlined;
+EditSimpleOutlined.displayName = 'EditSimpleOutlined';
+export default EditSimpleOutlined;
