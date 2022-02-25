3e6abdab90ddb7d3fa47a11c6bb1cc67589b7f0f
VeigarChen-Spotec <veigar.c@spotec.net>
2022-02-25 14:46:06 +0800

    fix: code review

--- packages/assui/src/label-input/demo/index.tsx
+++ packages/assui/src/label-input/demo/index.tsx
@@ -2,13 +2,15 @@ import React from 'react';
 import { Form, Button } from 'antd';
 import { LabelInput } from 'assui';
 
+import styles from './index.less';
+
 const Demo = () => {
   const onFinish = (value: any) => {
     console.log('value', value);
   };
 
   return (
-    <Form onFinish={onFinish} initialValues={{ account: '2323' }}>
+    <Form className={styles.form} onFinish={onFinish} initialValues={{ account: '2323' }}>
       <Form.Item name="account" rules={[{ required: true }]}>
         <LabelInput label="账户" />
       </Form.Item>
