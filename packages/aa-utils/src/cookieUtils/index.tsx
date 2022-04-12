081c744c884a9fcefaa674ae3094717e6f1e7b73
JasonChen-Spotec <46470667+JasonChen-Spotec@users.noreply.github.com>
2022-04-12 19:01:20 +0800

    feat: add cookie utils

---
+++ packages/aa-utils/src/cookieUtils/index.tsx
@@ -0,0 +1,7 @@
+import Cookies from 'universal-cookie';
+
+/** https://github.com/reactivestack/cookies/edit/master/packages/universal-cookie/README.md */
+
+const cookieUtil = new Cookies();
+
+export default cookieUtil;
