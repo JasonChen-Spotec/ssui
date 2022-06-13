20234bca0c83def9aec1b5b3c2b796ea57895177
Ella Li <ella.lee@spotec.net>
2022-06-13 15:19:59 +0800

    fix: icons fix invite friend color

--- packages/a-icons/src/InviteFriendFilled.tsx
+++ packages/a-icons/src/InviteFriendFilled.tsx
@@ -8,10 +8,8 @@ function InviteFriendFilled(componentProps: IconProps) {
         d="M3 19.5h12V18a3.75 3.75 0 00-3.75-3.75h-4.5A3.75 3.75 0 003 18v1.5zM17.88 8.398h1.44v7.2h-1.44v-7.2z"
         fill="currentColor"
       />
-      <path
-        d="M22.2 11.278v1.44H15v-1.44h7.2zM9 12.6a4.2 4.2 0 110-8.4 4.2 4.2 0 010 8.4z"
-        fill="currentColor"
-      />
+      <path d="M22.2 11.278v1.44H15v-1.44h7.2z" fill="currentColor" />
+      <path d="M9 12.6a4.2 4.2 0 110-8.4 4.2 4.2 0 010 8.4z" fill="#FF7612" />
     </svg>
   );
 
