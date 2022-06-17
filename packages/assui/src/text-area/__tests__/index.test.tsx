d2d4ece95a5dd7548ad63569dcb75bfef6617103
VeigarChen-Spotec <veigar.c@spotec.net>
2022-06-17 15:26:11 +0800

    feat: FEE-6 text area ut

---
+++ packages/assui/src/text-area/__tests__/index.test.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { render, fireEvent, screen } from '@testing-library/react';
+import TextArea from '../index';
+
+describe('TextArea', () => {
+  it('There should be no spaces before', () => {
+    const Demo = () => {
+      const [val, setVal] = React.useState('');
+      return <TextArea value={val} onChange={(value) => setVal(value)} />;
+    };
+
+    const { getByRole } = render(<Demo />);
+    const textArea = getByRole('textbox') as HTMLInputElement;
+    fireEvent.change(textArea, { target: { value: '    light' } });
+    expect(textArea.value).toBe('light');
+  });
+
+  it('has formatter', () => {
+    const Demo = () => {
+      const [val, setVal] = React.useState('');
+      return (
+        <TextArea
+          value={val}
+          onChange={(value) => setVal(value)}
+          formatter={(value) => value.toUpperCase()}
+        />
+      );
+    };
+
+    const { getByRole } = render(<Demo />);
+    const textArea = getByRole('textbox') as HTMLInputElement;
+    fireEvent.change(textArea, { target: { value: '    light' } });
+    expect(textArea.value).toBe('LIGHT');
+  });
+
+  it('uncontrolled', () => {
+    const { getByRole, rerender } = render(<TextArea />);
+    const textArea = getByRole('textbox') as HTMLInputElement;
+    fireEvent.change(textArea, { target: { value: '    light' } });
+    rerender(<TextArea value="light" />);
+  });
+});
