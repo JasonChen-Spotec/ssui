bdbe31da5f3f179828ce4ced18ce2d0e265ddae9
VeigarChen-Spotec <veigar.c@spotec.net>
2022-06-24 11:49:01 +0800

    feat: FEE-6 label select ut

--- packages/assui/src/label-select/__tests__/index.test.tsx
+++ packages/assui/src/label-select/__tests__/index.test.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { render, fireEvent, waitFor } from '@testing-library/react';
+import '@testing-library/jest-dom';
 import LabelSelect from '../index';
 
 const baseProps = {
   setOpen: jest.fn(),
+  onChange: jest.fn(),
 };
 
 const options = [
@@ -11,19 +13,35 @@ const options = [
     label: 'jack',
     value: 'jack',
   },
+  {
+    label: 'lucy',
+    value: 'lucy',
+  },
 ];
 
 describe('label-select', () => {
   it('label-select should work fine ', async () => {
-    const { getByRole, rerender, container, debug } = render(
-      <LabelSelect {...baseProps} options={options} />,
+    const { getByTitle, container } = render(
+      <LabelSelect {...baseProps} label="我是标题" options={options} />,
     );
+
     const labelNode = container.querySelector('.label-select-text') as HTMLLabelElement;
+
+    expect(labelNode).toHaveTextContent('我是标题');
+
     await waitFor(() => {
       fireEvent.click(labelNode);
     });
 
-    expect(container.querySelector('.ant-select-open')).toBeTruthy;
+    expect(container.querySelector('.ant-select-open')).toBeTruthy();
     expect(baseProps.setOpen).toBeCalledWith(true);
+
+    await waitFor(() => {
+      fireEvent.click(getByTitle('lucy'));
+    });
+
+    expect(container.querySelector('.ant-select-open')).toBeFalsy();
+    expect(container.querySelector('.ant-select-selection-item')).toHaveTextContent('lucy');
+    expect(baseProps.onChange).toBeCalledWith('lucy');
   });
 });
