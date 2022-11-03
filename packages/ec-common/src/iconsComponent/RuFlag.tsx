6706f7689e83559e4fa14b677f143a78ac9ee128
JasonChen-Spotec <46470667+JasonChen-Spotec@users.noreply.github.com>
2022-11-03 16:30:45 +0800

    feat(ec-common): CRMTMD-4969 俄罗斯国旗换立陶宛

--- packages/ec-common/src/iconsComponent/RuFlag.tsx
+++ packages/ec-common/src/iconsComponent/RuFlag.tsx
@@ -2,23 +2,14 @@ import * as React from 'react';
 import { SVGProps } from 'react';
 const SvgRuFlag = (props: SVGProps<SVGSVGElement>) => (
   <svg
-    width={26}
+    width={25}
     height={18}
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
     role="img"
     {...props}
   >
-    <rect
-      x={0.25}
-      y={0.25}
-      width={25.022}
-      height={17.5}
-      rx={1.75}
-      fill="#fff"
-      stroke="#F5F5F5"
-      strokeWidth={0.5}
-    />
+    <rect width={25} height={18} rx={2} fill="#fff" />
     <mask
       id="RUFlag_svg__a"
       style={{
@@ -27,23 +18,15 @@ const SvgRuFlag = (props: SVGProps<SVGSVGElement>) => (
       maskUnits="userSpaceOnUse"
       x={0}
       y={0}
-      width={26}
+      width={25}
       height={18}
     >
-      <rect
-        x={0.25}
-        y={0.25}
-        width={25.022}
-        height={17.5}
-        rx={1.75}
-        fill="#fff"
-        stroke="#fff"
-        strokeWidth={0.5}
-      />
+      <rect width={25} height={18} rx={2} fill="#fff" />
     </mask>
     <g mask="url(#RUFlag_svg__a)" fillRule="evenodd" clipRule="evenodd">
-      <path d="M0 12h25.522V6H0v6Z" fill="#0C47B7" />
-      <path d="M0 18h25.522v-6H0v6Z" fill="#E53B35" />
+      <path d="M0 12h25V6H0v6Z" fill="#118357" />
+      <path d="M0 18h25v-6H0v6Z" fill="#D8343D" />
+      <path d="M0 6h25V0H0v6Z" fill="#FEC34B" />
     </g>
   </svg>
 );
