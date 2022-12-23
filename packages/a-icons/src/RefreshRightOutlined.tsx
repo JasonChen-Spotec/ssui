380d764fba60b4c4957db1fcd1485364014f6c4c
TinnerLee-Spotec <84693560+TinnerLee-Spotec@users.noreply.github.com>
2022-12-23 16:29:25 +0800

    feat: 添加refreshRight图标 (#129)

---
+++ packages/a-icons/src/RefreshRightOutlined.tsx
@@ -0,0 +1,21 @@
+import * as React from 'react';
+import Icon, { IconProps } from 'a-base-icon/lib/icon';
+
+function RefreshRightOutlined(componentProps: IconProps) {
+  const IconNode = (props: React.SVGProps<SVGSVGElement>) => (
+    <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+      <path
+        d="M18.92 14.89c.26-.87.43-1.83.43-2.89 0-4.79-3.88-8.67-8.67-8.67C5.88 3.33 2 7.21 2 12s3.88 8.67 8.67 8.67c1.78 0 3.44-.54 4.82-1.46M18.68 16.13L22 13.24M18.68 16.13l-2.46-3.37"
+        stroke="currentColor"
+        strokeWidth={1.5}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+
+  return <Icon {...componentProps} component={IconNode} />;
+}
+
+RefreshRightOutlined.displayName = 'RefreshRightOutlined';
+export default RefreshRightOutlined;
