6c059bcc0516d976c9ba3971d1ddd3ed00249c38
JasonChen-Spotec <46470667+JasonChen-Spotec@users.noreply.github.com>
2023-04-21 14:27:44 +0800

    feat(a-icons): add icons

--- packages/a-icons/src/SquareUpRightOutlined.tsx
+++ packages/a-icons/src/SquareUpRightOutlined.tsx
@@ -3,7 +3,7 @@ import Icon, { IconProps } from 'a-base-icon/lib/icon';
 
 function SquareUpRightOutlined(componentProps: IconProps) {
   const IconNode = (props: React.SVGProps<SVGSVGElement>) => (
-    <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+    <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" {...props}>
       <path
         d="M15.58 11.39a.79.79 0 00.53.22.77.77 0 00.53-.22l2.93-2.93a.72.72 0 00.16-.25.71.71 0 000-.57.779.779 0 00-.16-.24l-2.93-2.93a.75.75 0 10-1.06 1.06l1.65 1.65h-7.09a5.2 5.2 0 00-5.2 5.2v2.28a5.11 5.11 0 005 5.09h7.18a.76.76 0 00.78-.75.75.75 0 00-.74-.75H10a3.69 3.69 0 01-3.58-3.7v-2.17a3.7 3.7 0 013.7-3.7h7.09l-1.65 1.65a.75.75 0 00.02 1.06z"
         fill="currentColor"
