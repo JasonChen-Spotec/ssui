05febd5cc259b9b63fbd700bf8fa384878f983b6
JasonChen-Spotec <46470667+JasonChen-Spotec@users.noreply.github.com>
2023-06-14 16:12:36 +0800

    feat: 加入 label range number 组件

---
+++ packages/assui/src/label-range-number/index.md
@@ -0,0 +1,22 @@
+---
+mobile: false
+title: LabelRangeNumber
+nav:
+  title: assui
+  path: /assui
+group:
+  title: 数据录入
+  path: /components/input
+---
+
+# LabelRangeNumber
+
+输入 LabelRangeNumber 组件
+
+## 1. 代码演示
+
+### 1.1. 基础使用
+
+<code hideActions='["CSB", "EXTERNAL"]' src="./demo/index.tsx" />
+
+<API></API>
---
+++ packages/assui/src/label-range-number/index.tsx
@@ -0,0 +1,131 @@
+import React from 'react';
+import useControllableValue from 'ahooks/lib/useControllableValue';
+import useClickAway from 'ahooks/lib/useClickAway';
+import classNames from 'classnames';
+import type { NumberInputProps } from '../number-input';
+import NumberInput from '../number-input';
+
+export type RangeNumberValue = {
+  startInterval?: number | string;
+  endInterval?: number | string;
+};
+
+export interface RangeNumberProps {
+  className?: string;
+  disabled?: boolean;
+  label: React.ReactNode;
+  value?: RangeNumberValue;
+  onChange?: (value: RangeNumberValue) => void;
+  enableMinus?: boolean;
+  numberType?: 'int' | 'float';
+  precision?: number;
+  connector?: React.ReactNode;
+  suffix?: string;
+  /** value的数据类型 */
+  dataType?: 'number' | 'string';
+  startNumberInputProps?: NumberInputProps;
+  endNumberInputProps?: NumberInputProps;
+}
+
+const defaultValue = {
+  startInterval: '',
+  endInterval: '',
+};
+
+const LabelDatePicker: React.FC<RangeNumberProps> = (props) => {
+  const {
+    disabled,
+    className,
+    label,
+    enableMinus,
+    numberType,
+    dataType,
+    precision,
+    connector,
+    startNumberInputProps,
+    endNumberInputProps,
+  } = props;
+  const containerRef = React.useRef<any>(null);
+  const startNumberRef = React.useRef<any>(null);
+  const [focus, onFocus] = useControllableValue(props, {
+    valuePropName: 'focus',
+    trigger: 'onFocus',
+  });
+
+  const [value, onChangeValue] = useControllableValue(props, {
+    defaultValue: {
+      startInterval: '',
+      endInterval: '',
+    },
+  });
+
+  useClickAway(() => {
+    onFocus(false);
+  }, containerRef);
+
+  const onStartChange = (startInterval: any) => {
+    onChangeValue({ ...value, startInterval });
+  };
+
+  const onEndChange = (endInterval: any) => {
+    onChangeValue({ ...value, endInterval });
+  };
+
+  const handleLabelClick = () => {
+    if (!focus) {
+      onFocus(!focus);
+    }
+    startNumberRef.current?.focus();
+  };
+
+  const resultValue = value || defaultValue;
+
+  return (
+    <div
+      ref={containerRef}
+      className={classNames(
+        {
+          'label-range-number': true,
+          'label-range-number-disabled': disabled,
+          'label-range-number-label-scale':
+            focus || resultValue.endInterval || resultValue.startInterval,
+        },
+        className,
+      )}
+    >
+      <div className="a-number a-number-range a-number-focused" aria-required="true">
+        <div className="a-number-input">
+          <NumberInput
+            bordered={false}
+            enableMinus={enableMinus}
+            numberType={numberType}
+            precision={precision}
+            dataType={dataType}
+            {...endNumberInputProps}
+            ref={startNumberRef}
+            onChange={onStartChange}
+            value={resultValue.startInterval}
+          />
+        </div>
+        <div className="a-number-range-separator">{connector || '–'}</div>
+        <div className="a-number-input">
+          <NumberInput
+            bordered={false}
+            enableMinus={enableMinus}
+            numberType={numberType}
+            precision={precision}
+            dataType={dataType}
+            {...startNumberInputProps}
+            onChange={onEndChange}
+            value={resultValue.endInterval}
+          />
+        </div>
+      </div>
+      <label className="label-range-number-text" onClick={handleLabelClick}>
+        {label}
+      </label>
+    </div>
+  );
+};
+
+export default LabelDatePicker;
