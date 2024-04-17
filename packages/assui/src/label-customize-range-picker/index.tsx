8a224e91930ff5b4c89d96bd493e208ab7a3eab9
Tinner <tinner.lee@spotec.net>
2024-04-17 10:57:45 +0800

    修复LabelCustomizeRangePicker在showTime且maxScope时的时间选择问题

--- packages/assui/src/label-customize-range-picker/index.tsx
+++ packages/assui/src/label-customize-range-picker/index.tsx
@@ -7,7 +7,6 @@ import type { RangeValue } from 'rc-picker/lib/interface';
 import type { CheckboxChangeEvent } from 'antd/lib/checkbox';
 import useControllableValue from 'ahooks/lib/useControllableValue';
 import type { Moment } from 'moment';
-import moment from 'moment';
 import omit from 'lodash/omit';
 import type { dateTypeEnum } from './defaultRadioList';
 import getDefaultRadioList from './defaultRadioList';
@@ -22,6 +21,12 @@ export type RadioListType = {
   text: string;
   value: [Moment, Moment];
 };
+
+/** 1整天的毫秒数 */
+const ONE_DAY_MILLISECOND = 1000 * 60 * 60 * 24;
+
+/** 1分钟的毫秒数 */
+const ONE_MINUTE_MILLISECOND = 1000 * 60;
 
 export interface LabelCustomizeRangePickerProps
   extends Omit<LabelRangePickerProps, 'label'> {
@@ -112,13 +117,15 @@ const LabelCustomizeRangePicker = (props: LabelCustomizeRangePickerProps) => {
     setIsVisiblePanel(checked);
   };
 
-  const onDateChange = (nextValue: RangeValue<moment.Moment>) => {
+  const onDateChange = (nextValue: RangeValue<Moment>) => {
     console.log('nextValue', nextValue);
 
     const [start, end] = nextValue || [];
     let nextStartDate =
-      (showTime ? start?.clone() : start?.clone().startOf('day')) ?? null;
-    let nextEndDate = (showTime ? end?.clone() : end?.clone().endOf('day')) ?? null;
+      (showTime ? start?.clone().startOf('minute') : start?.clone().startOf('day')) ??
+      null;
+    let nextEndDate =
+      (showTime ? end?.clone().endOf('minute') : end?.clone().endOf('day')) ?? null;
     if (!nextStartDate && !nextEndDate) {
       return setDate(undefined);
     }
@@ -127,33 +134,59 @@ const LabelCustomizeRangePicker = (props: LabelCustomizeRangePickerProps) => {
       return setDate([nextStartDate, nextEndDate]);
     }
 
+    const timeDiffOfShowTime = maxScope * ONE_DAY_MILLISECOND - ONE_MINUTE_MILLISECOND;
     const [startDate] = date || [];
+    // 1. start和end都存在时
     if (nextStartDate && nextEndDate) {
-      const isChangeStartData = !startDate?.clone().isSame(nextStartDate, 'day');
+      const isChangeStartData = !startDate?.clone().isSame(nextStartDate);
+      const momentDiffDay = nextEndDate.diff(nextStartDate) / ONE_DAY_MILLISECOND;
 
+      // 更改start
       if (isChangeStartData) {
-        if (nextEndDate.diff(nextStartDate, 'day') > maxScope) {
-          nextEndDate = nextStartDate
+        // 时间差大于maxScope，则将end调整到期望范围
+        if (momentDiffDay > maxScope) {
+          nextEndDate = showTime
+            ? nextStartDate
+                .clone()
+                .add(timeDiffOfShowTime, 'milliseconds')
+                .endOf('minute')
+            : nextStartDate
+                .clone()
+                .add(maxScope - 1, 'day')
+                .endOf('day');
+        }
+        // 更改end
+        // 时间差大于maxScope，则将结start调整到期望范围
+      } else if (momentDiffDay > maxScope) {
+        nextStartDate = showTime
+          ? nextEndDate
+              .clone()
+              .subtract(timeDiffOfShowTime, 'milliseconds')
+              .startOf('minute')
+          : nextEndDate
+              .clone()
+              .subtract(maxScope - 1, 'day')
+              .startOf('day');
+      }
+      // 2. 仅end存在时，自动填入start
+    } else if (!nextStartDate && nextEndDate) {
+      nextStartDate = showTime
+        ? nextEndDate
+            .clone()
+            .subtract(timeDiffOfShowTime, 'milliseconds')
+            .startOf('minute')
+        : nextEndDate
+            .clone()
+            .subtract(maxScope - 1, 'day')
+            .startOf('day');
+      // 3. 仅start存在时，自动填入end
+    } else if (nextStartDate && !nextEndDate) {
+      nextEndDate = showTime
+        ? nextStartDate.clone().add(timeDiffOfShowTime, 'milliseconds').endOf('minute')
+        : nextStartDate
             .clone()
             .add(maxScope - 1, 'day')
             .endOf('day');
-        }
-      } else if (nextEndDate.diff(nextStartDate, 'day') > maxScope) {
-        nextStartDate = nextEndDate
-          .clone()
-          .subtract(maxScope - 1, 'day')
-          .startOf('day');
-      }
-    } else if (!nextStartDate && nextEndDate) {
-      nextStartDate = nextEndDate
-        .clone()
-        .subtract(maxScope - 1, 'day')
-        .startOf('day');
-    } else if (nextStartDate && !nextEndDate) {
-      nextEndDate = nextStartDate
-        .clone()
-        .add(maxScope - 1, 'day')
-        .endOf('day');
     }
 
     if (nextStartDate && nextEndDate) {
@@ -175,7 +208,7 @@ const LabelCustomizeRangePicker = (props: LabelCustomizeRangePickerProps) => {
     ? dataSource.filter((item) => {
         const [startTime, entTime] = item.value;
 
-        const space = entTime.diff(startTime, 'days');
+        const space = entTime.diff(startTime) / ONE_DAY_MILLISECOND;
 
         return space <= maxScope;
       })
