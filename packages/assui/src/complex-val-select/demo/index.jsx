3b63f8cb1e33c97c77c7c9b52a919caf61813cfd
Tinner <tinner.lee@spotec.net>
2024-09-13 11:25:56 +0800

    ComplexvalSelect功能扩展&TODO处理

--- packages/assui/src/complex-val-select/demo/index.jsx
+++ packages/assui/src/complex-val-select/demo/index.jsx
@@ -7,13 +7,43 @@ const Demo = () => {
     console.log('Received values: ', values);
   };
 
+  const complexValOptions = [
+    {
+      label: 'Jack',
+      options: [
+        {
+          label: '杰克',
+          value: { spell: 'jieke', voice: 1 },
+        },
+        {
+          label: '捷克',
+          value: { spell: 'jieke', voice: 2 },
+        },
+      ],
+    },
+    {
+      label: 'Lucy',
+      options: [
+        {
+          label: 'lucy1',
+          value: { spell: 'luxi1', voice: 1 },
+          disabled: true,
+        },
+        {
+          label: 'lucy2',
+          value: { spell: 'luxi2', voice: 2 },
+        },
+      ],
+    },
+  ];
+
   return (
     <Form onFinish={onFinish}>
       <Form.Item name="primary" label="普通用法">
         <ComplexValSelect
           options={[
-            { label: '张三', value: 'zhangsan' },
-            { label: '李四', value: 'lisi' },
+            { label: '张三', value: 0 },
+            { label: '李四', value: 1 },
           ]}
         />
       </Form.Item>
@@ -32,6 +62,9 @@ const Demo = () => {
             { label: '李四信息', value: { name: '李四', sex: '女' } },
           ]}
         />
+      </Form.Item>
+      <Form.Item name="options" label="更复杂的下拉数据">
+        <ComplexValSelect options={complexValOptions} />
       </Form.Item>
       <Form.Item>
         <Button type="primary" htmlType="submit">
