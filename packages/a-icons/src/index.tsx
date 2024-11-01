7643279fa93a1edde3363d8547b63e6720309a52
Ella-Spotec <ella.lee@spotec.net>
2024-11-01 16:30:01 +0800

    feat: 新增countUp组件

--- packages/a-icons/src/index.tsx
+++ packages/a-icons/src/index.tsx
@@ -239,6 +239,7 @@ export { default as PeopleQuestionFilled } from './PeopleQuestionFilled';
 export { default as PeopleSettingFilled } from './PeopleSettingFilled';
 export { default as PeopleTeamFilled } from './PeopleTeamFilled';
 export { default as PeopleTeamOutlined } from './PeopleTeamOutlined';
+export { default as PersonWarningFilled } from './PersonWarningFilled';
 export { default as PhoneOutlined } from './PhoneOutlined';
 export { default as PlayFilled } from './PlayFilled';
 export { default as PlusFilled } from './PlusFilled';
