775d509c30242fc843d8bbd3946579523bd8c40f
LinkPeng-Spotec <link.peng@spotec.net>
2024-12-18 10:13:45 +0800

    feat: CRMTMD-13340 新增checkout closeout icon图标

--- packages/a-icons/src/CloseOutlined.tsx
+++ packages/a-icons/src/CloseOutlined.tsx
@@ -3,9 +3,11 @@ import Icon, { IconProps } from 'a-base-icon/lib/icon';
 
 function CloseOutlined(componentProps: IconProps) {
   const IconNode = (props: React.SVGProps<SVGSVGElement>) => (
-    <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" {...props}>
+    <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
       <path
-        d="M13.166 12.008l7.596-7.57a.825.825 0 000-1.16.81.81 0 00-1.15-.021l-7.596 7.57-7.498-7.57a.81.81 0 00-1.182 0 .814.814 0 000 1.094l7.498 7.559-7.596 7.558a.825.825 0 000 1.16.789.789 0 00.575.24.831.831 0 00.608-.196l7.595-7.57 7.596 7.657a.79.79 0 00.575.241.8.8 0 00.575-.24.825.825 0 000-1.16l-7.596-7.592z"
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M6 4.586l6 6 6-6L19.414 6l-6 6 6 6L18 19.414l-6-6-6 6L4.586 18l6-6-6-6L6 4.586z"
         fill="currentColor"
       />
     </svg>
