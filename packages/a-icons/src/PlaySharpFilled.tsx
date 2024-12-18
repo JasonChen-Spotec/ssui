b1214c886e02b3bc6ae51271ac795ff0d4085ad9
Tinner <tinner.lee@spotec.net>
2024-12-18 09:43:06 +0800

    暂停图标更名为播放

--- packages/a-icons/src/PlaySharpFilled.tsx
+++ packages/a-icons/src/PlaySharpFilled.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import Icon, { IconProps } from 'a-base-icon/lib/icon';
 
-function PauseFilled(componentProps: IconProps) {
+function PlaySharpFilled(componentProps: IconProps) {
   const IconNode = (props: React.SVGProps<SVGSVGElement>) => (
     <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
       <path
@@ -16,5 +16,5 @@ function PauseFilled(componentProps: IconProps) {
   return <Icon {...componentProps} component={IconNode} />;
 }
 
-PauseFilled.displayName = 'PauseFilled';
-export default PauseFilled;
+PlaySharpFilled.displayName = 'PlaySharpFilled';
+export default PlaySharpFilled;
