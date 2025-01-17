ea53b8c30b93fad05d58f9955db9714a38270ac0
VeigarChen-Spotec <veigar.c@spotec.net>
2025-01-17 16:28:09 +0800

    feat: add person left arrow icon

--- packages/a-icons/src/index.tsx
+++ packages/a-icons/src/index.tsx
@@ -244,6 +244,7 @@ export { default as PeopleSettingFilled } from './PeopleSettingFilled';
 export { default as PeopleTeamFilled } from './PeopleTeamFilled';
 export { default as PeopleTeamOutlined } from './PeopleTeamOutlined';
 export { default as PersonGroupFilled } from './PersonGroupFilled';
+export { default as PersonLeftArrowFilled } from './PersonLeftArrowFilled';
 export { default as PersonWarningFilled } from './PersonWarningFilled';
 export { default as PhoneOutlined } from './PhoneOutlined';
 export { default as PlayFilled } from './PlayFilled';
