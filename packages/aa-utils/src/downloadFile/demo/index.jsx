70fdfaca42399eeda41a77ddeb8ea36c8c42ad83
LinkPeng-Spotec <link.peng@spotec.net>
2025-02-06 18:45:14 +0800

    feat: CRMTMD-15466 增加download自定义文件名称的扩展名 (#274)

--- packages/aa-utils/src/downloadFile/demo/index.jsx
+++ packages/aa-utils/src/downloadFile/demo/index.jsx
@@ -23,11 +23,20 @@ const Demo = () => {
     );
   };
 
+  const onClickExcel = () => {
+    downloadFile(
+      'http://192.168.0.126:9000/s3-crm-s/report/5c79cc2fee3e6437e8597a6eddc329cd.zip?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=minio/20250206/us-east-1/s3/aws4_request&X-Amz-Date=20250206T102945Z&X-Amz-Expires=3600&X-Amz-SignedHeaders=host&X-Amz-Signature=5c9828b80b37dd48e98d997ee40c3288f7392bfa088f18d807e08f3ad539e595',
+      {
+        fileName: `zip格式`,
+      },
+    );
+  };
+
   return (
     <Space>
       <Button onClick={onClick}>下载</Button>
-
       <Button onClick={onClickHaveFileName}>下载2</Button>
+      <Button onClick={onClickExcel}>下载其他格式</Button>
     </Space>
   );
 };
