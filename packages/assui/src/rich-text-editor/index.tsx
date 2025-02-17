04889d5ae1873cc693539883ca1861f474ddcc24
Ian <lan.tang@spotec.net>
2025-02-17 17:12:27 +0800

    feat: RichTextEditor 添加skip: false

--- packages/assui/src/rich-text-editor/index.tsx
+++ packages/assui/src/rich-text-editor/index.tsx
@@ -80,6 +80,7 @@ const defaultInit = {
   toolbar: defaultToolbar,
   content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }',
   font_size_input_default_unit: 'px',
+  skip: false,
   font_size_formats:
     '10px 12px 14px 16px 18px 20px 22px 24px 26px 28px 30px 32px 34px 36px',
 };
