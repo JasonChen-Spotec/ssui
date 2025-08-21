32bd8d4062b9e714507b712985a03d511a24acd2
Ella-Spotec <ella.lee@spotec.net>
2025-08-21 18:34:40 +0800

    feat: 新增映射新增icon

---
+++ packages/a-icons/lib/MappingAddOutlined.d.ts
@@ -0,0 +1,6 @@
+import { IconProps } from 'a-base-icon/lib/icon';
+declare function MappingAddOutlined(componentProps: IconProps): JSX.Element;
+declare namespace MappingAddOutlined {
+    var displayName: string;
+}
+export default MappingAddOutlined;
