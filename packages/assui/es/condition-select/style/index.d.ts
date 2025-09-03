5fc9fa818000cffeb3a9f55c04ebc68f0e7d625d
TerrinTian-Spotec <terrin.tian@spotec.net>
2025-09-03 09:59:30 +0800

    feat: SCRUM-3043 ConditionSelect style文件引入

---
+++ packages/assui/es/condition-select/style/index.d.ts
@@ -0,0 +1,3 @@
+import 'antd/lib/col/style';
+import 'antd/lib/row/style';
+import 'antd/lib/select/style';
---
+++ packages/assui/lib/condition-select/style/index.js
@@ -0,0 +1,8 @@
+'use strict';
+
+Object.defineProperty(exports, '__esModule', {
+  value: true,
+});
+require('antd/lib/select/style');
+require('antd/lib/row/style');
+require('antd/lib/col/style');
