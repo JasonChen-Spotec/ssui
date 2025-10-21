1eabf0aa7c007bfdc2718fffb61c7006b0cf888f
IanTang-Spotec <lan.tang@spotec.net>
2025-10-21 10:51:11 +0800

    feat: SingleImgUpload 适配pdf

--- packages/ec-common/webpack.config.js
+++ packages/ec-common/webpack.config.js
@@ -9,4 +9,15 @@ module.exports = merge(common, {
     library: 'assui',
     path: path.resolve(__dirname, './dist'),
   },
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i, // 👈 匹配图片文件
+        type: 'asset/resource', // 👈 使用 asset module 自动生成文件 URL
+        generator: {
+          filename: 'assets/[name].[hash:8][ext]', // 👈 输出路径格式
+        },
+      },
+    ],
+  },
 });
