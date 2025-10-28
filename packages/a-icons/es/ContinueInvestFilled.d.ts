1e0a4889ee9cb9dfb84d075a06e2f358b98b2d2c
ZackZeng-Spotec <zack.zeng@spotec.net>
2025-10-28 17:32:55 +0800

    feat: SCRUM-3535 PAMM-合约-已暂停-继续投资/批量继续投资/增加相关图标 (#332)

---
+++ packages/a-icons/es/ContinueInvestFilled.d.ts
@@ -0,0 +1,6 @@
+import { IconProps } from 'a-base-icon/lib/icon';
+declare function ContinueInvestFilled(componentProps: IconProps): JSX.Element;
+declare namespace ContinueInvestFilled {
+    var displayName: string;
+}
+export default ContinueInvestFilled;
