022a81f0a3563e3e3562140a00d5e41894521e84
TinnerLee-Spotec <tinner.lee@spotec.net>
2025-11-04 14:08:57 +0800

    新增LightbulbOutlined图标

---
+++ packages/a-icons/es/LightbulbOutlined.d.ts
@@ -0,0 +1,6 @@
+import { IconProps } from 'a-base-icon/lib/icon';
+declare function LightbulbOutlined(componentProps: IconProps): JSX.Element;
+declare namespace LightbulbOutlined {
+    var displayName: string;
+}
+export default LightbulbOutlined;
---
+++ packages/a-icons/src/LightbulbOutlined.tsx
@@ -0,0 +1,21 @@
+import * as React from 'react';
+import Icon, { IconProps } from 'a-base-icon/lib/icon';
+
+function LightbulbOutlined(componentProps: IconProps) {
+  const IconNode = (props: React.SVGProps<SVGSVGElement>) => (
+    <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+      <path
+        d="M9.5 22h5M10 10h4m-2 0v6m3-.674a7 7 0 10-6 0V16c0 .932 0 1.398.152 1.765a2 2 0 001.083 1.082C10.602 19 11.068 19 12 19c.932 0 1.398 0 1.765-.152a2 2 0 001.083-1.083C15 17.398 15 16.932 15 16v-.674z"
+        stroke="currentCOlor"
+        strokeWidth={2}
+        strokeLinecap="square"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+
+  return <Icon {...componentProps} component={IconNode} />;
+}
+
+LightbulbOutlined.displayName = 'LightbulbOutlined';
+export default LightbulbOutlined;
