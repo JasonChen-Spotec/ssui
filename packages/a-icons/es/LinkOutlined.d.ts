2f7aece382d0547a56d47dd7d28289970310863f
Reed-Tang <Reed.tang@spotec.net>
2025-11-06 17:03:59 +0800

    feat: a-icons添加link svg

---
+++ packages/a-icons/es/LinkOutlined.d.ts
@@ -0,0 +1,6 @@
+import { IconProps } from 'a-base-icon/lib/icon';
+declare function LinkOutlined(componentProps: IconProps): JSX.Element;
+declare namespace LinkOutlined {
+    var displayName: string;
+}
+export default LinkOutlined;
