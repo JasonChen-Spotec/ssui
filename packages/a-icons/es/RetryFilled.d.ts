9cf738d8c0bb0f369887aacade0fee756e2aa086
LinkPeng-Spotec <link.peng@spotec.net>
2025-11-18 17:27:15 +0800

    feat: SCRUM-4811 重试icon添加

---
+++ packages/a-icons/es/RetryFilled.d.ts
@@ -0,0 +1,6 @@
+import { IconProps } from 'a-base-icon/lib/icon';
+declare function RetryFilled(componentProps: IconProps): JSX.Element;
+declare namespace RetryFilled {
+    var displayName: string;
+}
+export default RetryFilled;
