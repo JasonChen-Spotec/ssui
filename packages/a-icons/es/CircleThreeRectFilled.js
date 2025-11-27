25c58d8acfbd06c52780b03ff66d63434d454675
veigar <veigar.c@spotec.net>
2025-11-27 14:28:41 +0800

    feat: add circle three rect

---
+++ packages/a-icons/es/CircleThreeRectFilled.js
@@ -0,0 +1,32 @@
+var __assign = this && this.__assign || function () {
+  __assign = Object.assign || function (t) {
+    for (var s, i = 1, n = arguments.length; i < n; i++) {
+      s = arguments[i];
+      for (var p in s) {
+        if (Object.prototype.hasOwnProperty.call(s, p)) t[p] = s[p];
+      }
+    }
+    return t;
+  };
+  return __assign.apply(this, arguments);
+};
+import * as React from 'react';
+import Icon from 'a-base-icon/lib/icon';
+function CircleThreeRectFilled(componentProps) {
+  var IconNode = function IconNode(props) {
+    return /*#__PURE__*/React.createElement("svg", __assign({
+      viewBox: "0 0 24 24",
+      xmlns: "http://www.w3.org/2000/svg"
+    }, props), /*#__PURE__*/React.createElement("path", {
+      fillRule: "evenodd",
+      clipRule: "evenodd",
+      d: "M23 12c0 6.075-4.925 11-11 11S1 18.075 1 12 5.925 1 12 1s11 4.925 11 11zM7.4 13.1H5.2v-2.2h2.2v2.2zm3.5 0h2.2v-2.2h-2.2v2.2zm7.9 0h-2.2v-2.2h2.2v2.2z",
+      fill: "currentColor"
+    }));
+  };
+  return /*#__PURE__*/React.createElement(Icon, __assign({}, componentProps, {
+    component: IconNode
+  }));
+}
+CircleThreeRectFilled.displayName = 'CircleThreeRectFilled';
+export default CircleThreeRectFilled;
---
+++ packages/a-icons/src/CircleThreeRectFilled.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import Icon, { IconProps } from 'a-base-icon/lib/icon';
+
+function CircleThreeRectFilled(componentProps: IconProps) {
+  const IconNode = (props: React.SVGProps<SVGSVGElement>) => (
+    <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" {...props}>
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M23 12c0 6.075-4.925 11-11 11S1 18.075 1 12 5.925 1 12 1s11 4.925 11 11zM7.4 13.1H5.2v-2.2h2.2v2.2zm3.5 0h2.2v-2.2h-2.2v2.2zm7.9 0h-2.2v-2.2h2.2v2.2z"
+        fill="currentColor"
+      />
+    </svg>
+  );
+
+  return <Icon {...componentProps} component={IconNode} />;
+}
+
+CircleThreeRectFilled.displayName = 'CircleThreeRectFilled';
+export default CircleThreeRectFilled;
