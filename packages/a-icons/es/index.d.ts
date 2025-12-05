3563ff07cd4d9d98723328375098b57e04c09059
ReedTang-Spotec <Reed.tang@spotec.net>
2025-12-05 17:46:47 +0800

    feat: SCRUM-5088 添加面包屑的新样式图标LocationOutlined (#341)

--- packages/a-icons/es/index.d.ts
+++ packages/a-icons/es/index.d.ts
@@ -228,6 +228,7 @@ export { default as ListCaptionOutlined } from './ListCaptionOutlined';
 export { default as ListFilled } from './ListFilled';
 export { default as ListOutlined } from './ListOutlined';
 export { default as LoadingFilled } from './LoadingFilled';
+export { default as LocationOutlined } from './LocationOutlined';
 export { default as LockEllipsisFilled } from './LockEllipsisFilled';
 export { default as LockFilled } from './LockFilled';
 export { default as LockOutlined } from './LockOutlined';
