29f4ec36e0222af9d8d121ec954d94d0e99ef20f
ZackZeng-Spotec <zack.zeng@spotec.net>
2025-12-12 14:16:09 +0800

    feat: SCRUM-5256 打包assui

--- packages/aa-hooks/lib/index.js
+++ packages/aa-hooks/lib/index.js
@@ -3,17 +3,17 @@
 Object.defineProperty(exports, '__esModule', {
   value: true,
 });
-exports.generateUseDrawer = exports.useDrawer = void 0;
+exports.useDrawer = exports.generateUseDrawer = void 0;
 var useDrawer_1 = require('./useDrawer');
-Object.defineProperty(exports, 'useDrawer', {
-  enumerable: true,
-  get: function get() {
-    return useDrawer_1.useDrawer;
-  },
-});
 Object.defineProperty(exports, 'generateUseDrawer', {
   enumerable: true,
   get: function get() {
     return useDrawer_1.generateUseDrawer;
+  },
+});
+Object.defineProperty(exports, 'useDrawer', {
+  enumerable: true,
+  get: function get() {
+    return useDrawer_1.useDrawer;
   },
 });
