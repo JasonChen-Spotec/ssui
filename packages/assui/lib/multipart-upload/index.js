6c9b35cb9fb618eec2d9cfd454d3fab69f312b46
ZackZeng-Spotec <zack.zeng@spotec.net>
2025-12-12 14:06:53 +0800

    feat: SCRUM-5256 打包assui (#346)

--- packages/assui/lib/multipart-upload/index.js
+++ packages/assui/lib/multipart-upload/index.js
@@ -4,9 +4,7 @@ var __assign = this && this.__assign || function () {
   __assign = Object.assign || function (t) {
     for (var s, i = 1, n = arguments.length; i < n; i++) {
       s = arguments[i];
-      for (var p in s) {
-        if (Object.prototype.hasOwnProperty.call(s, p)) t[p] = s[p];
-      }
+      for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p)) t[p] = s[p];
     }
     return t;
   };
@@ -39,17 +37,13 @@ var __setModuleDefault = this && this.__setModuleDefault || (Object.create ? fun
 var __importStar = this && this.__importStar || function (mod) {
   if (mod && mod.__esModule) return mod;
   var result = {};
-  if (mod != null) for (var k in mod) {
-    if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
-  }
+  if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
   __setModuleDefault(result, mod);
   return result;
 };
 var __rest = this && this.__rest || function (s, e) {
   var t = {};
-  for (var p in s) {
-    if (Object.prototype.hasOwnProperty.call(s, p) && e.indexOf(p) < 0) t[p] = s[p];
-  }
+  for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p) && e.indexOf(p) < 0) t[p] = s[p];
   if (s != null && typeof Object.getOwnPropertySymbols === "function") for (var i = 0, p = Object.getOwnPropertySymbols(s); i < p.length; i++) {
     if (e.indexOf(p[i]) < 0 && Object.prototype.propertyIsEnumerable.call(s, p[i])) t[p[i]] = s[p[i]];
   }
@@ -63,9 +57,7 @@ var __read = this && this.__read || function (o, n) {
     ar = [],
     e;
   try {
-    while ((n === void 0 || n-- > 0) && !(r = i.next()).done) {
-      ar.push(r.value);
-    }
+    while ((n === void 0 || n-- > 0) && !(r = i.next()).done) ar.push(r.value);
   } catch (error) {
     e = {
       error: error
@@ -172,7 +164,7 @@ var MultipartUpload = function MultipartUpload(props) {
       setUploadPercent(currentProgressNumber);
     }
   };
-  var uploadNextChunk = function uploadNextChunk() {
+  var _uploadNextChunk = function uploadNextChunk() {
     /** 第二步 获取url 进行分片上传  */
     if (fileRef.current) {
       var resultUrl = getInProgressUploadUrl({
@@ -188,7 +180,7 @@ var MultipartUpload = function MultipartUpload(props) {
         uploadList.push(res);
         currentChunk += 1;
         if (currentChunk < totalChunks) {
-          uploadNextChunk();
+          _uploadNextChunk();
         } else {
           /** 分片上传完成 进行文件合并 上传完成  */
           completeRequest({
@@ -213,7 +205,7 @@ var MultipartUpload = function MultipartUpload(props) {
         fileName: fileNameRef.current
       }).then(function (id) {
         uploadIdRef.current = id;
-        uploadNextChunk();
+        _uploadNextChunk();
       });
     }
   };
