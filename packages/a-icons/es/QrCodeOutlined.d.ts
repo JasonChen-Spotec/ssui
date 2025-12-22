ad00955653221795125fa72b0cb3cd0ad1870e7b
TinnerLee-Spotec <tinner.lee@spotec.net>
2025-12-22 17:32:15 +0800

    添加线框二维码icon

---
+++ packages/a-icons/es/QrCodeOutlined.d.ts
@@ -0,0 +1,6 @@
+import { IconProps } from 'a-base-icon/lib/icon';
+declare function QrCodeOutlined(componentProps: IconProps): JSX.Element;
+declare namespace QrCodeOutlined {
+    var displayName: string;
+}
+export default QrCodeOutlined;
