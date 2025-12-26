c635169a27813ad941cf086fcdea4c57ef28f0bd
TinnerLee-Spotec <tinner.lee@spotec.net>
2025-12-26 15:29:51 +0800

    feat: SCRUM-6237 filled图标替换：个别图标调整

--- packages/a-icons/lib/PlaySharpFilled.d.ts
+++
@@ -1,6 +0,0 @@
-import { IconProps } from 'a-base-icon/lib/icon';
-declare function PlaySharpFilled(componentProps: IconProps): JSX.Element;
-declare namespace PlaySharpFilled {
-    var displayName: string;
-}
-export default PlaySharpFilled;
--- packages/a-icons/src/PlayOutlined.tsx
+++ packages/a-icons/src/PlayOutlined.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import Icon, { IconProps } from 'a-base-icon/lib/icon';
 
-function PlaySharpFilled(componentProps: IconProps) {
+function PlayOutlined(componentProps: IconProps) {
   const IconNode = (props: React.SVGProps<SVGSVGElement>) => (
     <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
       <path
@@ -14,5 +14,5 @@ function PlaySharpFilled(componentProps: IconProps) {
   return <Icon {...componentProps} component={IconNode} />;
 }
 
-PlaySharpFilled.displayName = 'PlaySharpFilled';
-export default PlaySharpFilled;
+PlayOutlined.displayName = 'PlayOutlined';
+export default PlayOutlined;
