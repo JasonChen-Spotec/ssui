1704ba43f58c89c3b271be21b67b3c9bec08aa6f
EllaLee-spotec <75291282+EllaLee-spotec@users.noreply.github.com>
2026-01-07 11:29:23 +0800

    feat: 新增补发icon (#359)

---
+++ packages/a-icons/es/TransferMoneyFilled.js
@@ -0,0 +1,36 @@
+var __assign = this && this.__assign || function () {
+  __assign = Object.assign || function (t) {
+    for (var s, i = 1, n = arguments.length; i < n; i++) {
+      s = arguments[i];
+      for (var p in s) {
+        if (Object.prototype.hasOwnProperty.call(s, p)) t[p] = s[p];
+      }
+    }
+    return t;
+  };
+  return __assign.apply(this, arguments);
+};
+import * as React from 'react';
+import Icon from 'a-base-icon/lib/icon';
+function TransferMoneyFilled(componentProps) {
+  var IconNode = function IconNode(props) {
+    return /*#__PURE__*/React.createElement("svg", __assign({
+      viewBox: "0 0 24 24",
+      fill: "none",
+      xmlns: "http://www.w3.org/2000/svg"
+    }, props), /*#__PURE__*/React.createElement("path", {
+      fillRule: "evenodd",
+      clipRule: "evenodd",
+      d: "M17 1a4 4 0 014 4v7.875A5.5 5.5 0 0016.705 23H7a4 4 0 01-4-4V5a4 4 0 014-4h10zm-5 5.121L9.879 4 8.465 5.414l2.12 2.121H8v2h3v1H8v2h3v2h2v-2h3v-2h-3v-1h3v-2h-2.586l2.121-2.12L14.121 4 12 6.121z",
+      fill: "currentColor"
+    }), /*#__PURE__*/React.createElement("path", {
+      d: "M22.707 17.293a1 1 0 010 1.414L19 22.414 17.586 21l2-2H15v-2h4.586l-2-2L19 13.586l3.707 3.707z",
+      fill: "currentColor"
+    }));
+  };
+  return /*#__PURE__*/React.createElement(Icon, __assign({}, componentProps, {
+    component: IconNode
+  }));
+}
+TransferMoneyFilled.displayName = 'TransferMoneyFilled';
+export default TransferMoneyFilled;
---
+++ packages/a-icons/lib/TransferMoneyFilled.js
@@ -0,0 +1,79 @@
+"use strict";
+
+var __assign = this && this.__assign || function () {
+  __assign = Object.assign || function (t) {
+    for (var s, i = 1, n = arguments.length; i < n; i++) {
+      s = arguments[i];
+      for (var p in s) {
+        if (Object.prototype.hasOwnProperty.call(s, p)) t[p] = s[p];
+      }
+    }
+    return t;
+  };
+  return __assign.apply(this, arguments);
+};
+var __createBinding = this && this.__createBinding || (Object.create ? function (o, m, k, k2) {
+  if (k2 === undefined) k2 = k;
+  var desc = Object.getOwnPropertyDescriptor(m, k);
+  if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+    desc = {
+      enumerable: true,
+      get: function get() {
+        return m[k];
+      }
+    };
+  }
+  Object.defineProperty(o, k2, desc);
+} : function (o, m, k, k2) {
+  if (k2 === undefined) k2 = k;
+  o[k2] = m[k];
+});
+var __setModuleDefault = this && this.__setModuleDefault || (Object.create ? function (o, v) {
+  Object.defineProperty(o, "default", {
+    enumerable: true,
+    value: v
+  });
+} : function (o, v) {
+  o["default"] = v;
+});
+var __importStar = this && this.__importStar || function (mod) {
+  if (mod && mod.__esModule) return mod;
+  var result = {};
+  if (mod != null) for (var k in mod) {
+    if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+  }
+  __setModuleDefault(result, mod);
+  return result;
+};
+var __importDefault = this && this.__importDefault || function (mod) {
+  return mod && mod.__esModule ? mod : {
+    "default": mod
+  };
+};
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+var React = __importStar(require("react"));
+var icon_1 = __importDefault(require("a-base-icon/lib/icon"));
+function TransferMoneyFilled(componentProps) {
+  var IconNode = function IconNode(props) {
+    return React.createElement("svg", __assign({
+      viewBox: "0 0 24 24",
+      fill: "none",
+      xmlns: "http://www.w3.org/2000/svg"
+    }, props), React.createElement("path", {
+      fillRule: "evenodd",
+      clipRule: "evenodd",
+      d: "M17 1a4 4 0 014 4v7.875A5.5 5.5 0 0016.705 23H7a4 4 0 01-4-4V5a4 4 0 014-4h10zm-5 5.121L9.879 4 8.465 5.414l2.12 2.121H8v2h3v1H8v2h3v2h2v-2h3v-2h-3v-1h3v-2h-2.586l2.121-2.12L14.121 4 12 6.121z",
+      fill: "currentColor"
+    }), React.createElement("path", {
+      d: "M22.707 17.293a1 1 0 010 1.414L19 22.414 17.586 21l2-2H15v-2h4.586l-2-2L19 13.586l3.707 3.707z",
+      fill: "currentColor"
+    }));
+  };
+  return React.createElement(icon_1["default"], __assign({}, componentProps, {
+    component: IconNode
+  }));
+}
+TransferMoneyFilled.displayName = 'TransferMoneyFilled';
+exports["default"] = TransferMoneyFilled;
