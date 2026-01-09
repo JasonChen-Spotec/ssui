813d829f9cac360186aa34f373b95c06d8e12e4b
TinnerLee-Spotec <84693560+TinnerLee-Spotec@users.noreply.github.com>
2026-01-09 14:21:36 +0800

    SCRUM-6198 添加MailSettingFilled (#362)

---
+++ packages/a-icons/lib/MailSettingFilled.d.ts
@@ -0,0 +1,6 @@
+import { IconProps } from 'a-base-icon/lib/icon';
+declare function MailSettingFilled(componentProps: IconProps): JSX.Element;
+declare namespace MailSettingFilled {
+    var displayName: string;
+}
+export default MailSettingFilled;
