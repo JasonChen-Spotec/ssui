6e4d3e69ef5adf480a5a9378fb0775f03029774d
TerrinTian-Spotec <terrin.tian@spotec.net>
2026-03-16 17:03:25 +0800

    feat: SCRUM-7696 登录-三方登录：Google (#379)

---
+++ packages/a-icons/lib/UnionOutlined.js
@@ -0,0 +1,76 @@
+"use strict";
+
+var __assign = this && this.__assign || function () {
+  __assign = Object.assign || function (t) {
+    for (var s, i = 1, n = arguments.length; i < n; i++) {
+      s = arguments[i];
+      for (var p in s) {
+        if (Object.prototype.hasOwnProperty.call(s, p)) t[p] = s[p];
+      }
+    }
+    return t;
+  };
+  return __assign.apply(this, arguments);
+};
+var __createBinding = this && this.__createBinding || (Object.create ? function (o, m, k, k2) {
+  if (k2 === undefined) k2 = k;
+  var desc = Object.getOwnPropertyDescriptor(m, k);
+  if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+    desc = {
+      enumerable: true,
+      get: function get() {
+        return m[k];
+      }
+    };
+  }
+  Object.defineProperty(o, k2, desc);
+} : function (o, m, k, k2) {
+  if (k2 === undefined) k2 = k;
+  o[k2] = m[k];
+});
+var __setModuleDefault = this && this.__setModuleDefault || (Object.create ? function (o, v) {
+  Object.defineProperty(o, "default", {
+    enumerable: true,
+    value: v
+  });
+} : function (o, v) {
+  o["default"] = v;
+});
+var __importStar = this && this.__importStar || function (mod) {
+  if (mod && mod.__esModule) return mod;
+  var result = {};
+  if (mod != null) for (var k in mod) {
+    if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+  }
+  __setModuleDefault(result, mod);
+  return result;
+};
+var __importDefault = this && this.__importDefault || function (mod) {
+  return mod && mod.__esModule ? mod : {
+    "default": mod
+  };
+};
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+var React = __importStar(require("react"));
+var icon_1 = __importDefault(require("a-base-icon/lib/icon"));
+function UnionOutlined(componentProps) {
+  var IconNode = function IconNode(props) {
+    return React.createElement("svg", __assign({
+      viewBox: "0 0 24 24",
+      xmlns: "http://www.w3.org/2000/svg"
+    }, props), React.createElement("path", {
+      d: "M19 8a3 3 0 013 3v8a3 3 0 01-3 3h-8a3 3 0 01-3-3v-2h2v2a1 1 0 001 1h8a1 1 0 001-1v-8a1 1 0 00-1-1h-8a1 1 0 00-1 1v2H8v-2a3 3 0 013-3h8z",
+      fill: "currentColor"
+    }), React.createElement("path", {
+      d: "M13 2a3 3 0 013 3v2h-2V5a1 1 0 00-1-1H5a1 1 0 00-1 1v8a1 1 0 001 1h8a1 1 0 001-1v-2h2v2a3 3 0 01-3 3H5a3 3 0 01-3-3V5a3 3 0 013-3h8z",
+      fill: "currentColor"
+    }));
+  };
+  return React.createElement(icon_1["default"], __assign({}, componentProps, {
+    component: IconNode
+  }));
+}
+UnionOutlined.displayName = 'UnionOutlined';
+exports["default"] = UnionOutlined;
