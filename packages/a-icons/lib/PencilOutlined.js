7a508d854256e955e32b7b9b3a47c1c88ace14d2
ZackZeng-Spotec <zack.zeng@spotec.net>
2026-04-22 18:52:51 +0800

    feat: SCRUM-8390 【客户端】【H5】个人中心-账户列表中关于icon大小不一致和“暂停支持”提示样式的优化 (#389)

--- packages/a-icons/lib/PencilOutlined.js
+++ packages/a-icons/lib/PencilOutlined.js
@@ -58,21 +58,17 @@ var icon_1 = __importDefault(require("a-base-icon/lib/icon"));
 function PencilOutlined(componentProps) {
   var IconNode = function IconNode(props) {
     return React.createElement("svg", __assign({
-      viewBox: "0 0 24 24",
+      width: 32,
+      height: 32,
+      fill: "none",
       xmlns: "http://www.w3.org/2000/svg"
-    }, props), React.createElement("g", {
-      clipPath: "url(#pencil-outlined_svg__clip0_248_810)"
-    }, React.createElement("path", {
-      fillRule: "evenodd",
-      clipRule: "evenodd",
-      d: "M17.793.793l5.414 5.414-1.414 1.414-5.414-5.414L17.793.793zM14.33 4.488a1.5 1.5 0 01.927 0c.232.075.4.2.512.295.096.081.196.182.28.266l.017.017L18.95 7.95c.084.084.185.184.266.28.094.111.22.28.296.513a1.5 1.5 0 010 .927 1.508 1.508 0 01-.296.512c-.081.096-.182.196-.266.28L8.6 20.814l-.038.038a4.665 4.665 0 01-.542.491 3.004 3.004 0 01-.538.312c-.215.097-.44.158-.695.227l-.052.014-4.68 1.277a1 1 0 01-1.228-1.228l1.277-4.68.014-.052c.069-.255.13-.48.227-.695a3 3 0 01.312-.538c.14-.19.304-.355.491-.542l.038-.038L13.52 5.066l.017-.017c.084-.084.184-.185.28-.266.111-.094.28-.22.512-.295zm.463 2.133L4.601 16.813c-.245.245-.291.295-.326.344a1 1 0 00-.105.18c-.024.054-.045.119-.136.453l-.816 2.992 2.992-.816c.334-.091.4-.112.454-.136.063-.029.123-.064.18-.104.048-.036.098-.082.343-.327L17.379 9.207l-2.586-2.586z",
-      fill: "currentColor"
-    })), React.createElement("defs", null, React.createElement("clipPath", {
-      id: "pencil-outlined_svg__clip0_248_810"
-    }, React.createElement("path", {
-      fill: "currentColor",
-      d: "M0 0h24v24H0z"
-    }))));
+    }, props), React.createElement("path", {
+      d: "M16.17 12.415l-6.817 6.818v3.409h3.408l6.818-6.818m-3.409-3.409l2.445-2.444.001-.002c.337-.336.505-.505.7-.568a.852.852 0 01.526 0c.194.063.363.232.699.568l1.482 1.482c.338.338.507.506.57.701a.852.852 0 010 .527c-.063.194-.232.363-.569.7h0l-2.445 2.445m-3.409-3.409l3.409 3.41",
+      stroke: "currentColor",
+      strokeWidth: 1.5,
+      strokeLinecap: "round",
+      strokeLinejoin: "round"
+    }));
   };
   return React.createElement(icon_1["default"], __assign({}, componentProps, {
     component: IconNode
