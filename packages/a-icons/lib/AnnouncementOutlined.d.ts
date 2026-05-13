a006d5b0665d6256d5921aade5cda8a23f36659f
LinkPeng-Spotec <link.peng@spotec.net>
2026-05-13 14:22:55 +0800

    feat: SCRUM-8953 新增菜单“分析”接入AcuityTrading (#397)

---
+++ packages/a-icons/lib/AnnouncementOutlined.d.ts
@@ -0,0 +1,6 @@
+import { IconProps } from 'a-base-icon/lib/icon';
+declare function AnnouncementOutlined(componentProps: IconProps): JSX.Element;
+declare namespace AnnouncementOutlined {
+    var displayName: string;
+}
+export default AnnouncementOutlined;
