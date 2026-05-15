5d83a2cb62e412ff5d37156dec0d3fb34d28ef4c
ZackZeng-Spotec <zack.zeng@spotec.net>
2026-05-15 16:18:49 +0800

    feat: SCRUM-9153 【管理端】系统-基本设置-Metatrader4/5：修改展示样式&添加“关联服务器”/加了个上传图标

---
+++ packages/a-icons/src/UploadOutlined.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import Icon, { IconProps } from 'a-base-icon/lib/icon';
+
+function UploadOutlined(componentProps: IconProps) {
+  const IconNode = (props: React.SVGProps<SVGSVGElement>) => (
+    <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M11.293 2.293a1 1 0 011.414 0L17.414 7 16 8.414l-3-3V16h-2V5.414l-3 3L6.586 7l4.707-4.707zM4 11v5.2c0 .857 0 1.439.038 1.889.035.438.1.663.18.819a2 2 0 00.874.874c.156.08.38.145.819.18C6.361 20 6.943 20 7.8 20h8.4c.857 0 1.439 0 1.889-.038.438-.035.663-.1.819-.18a2 2 0 00.874-.874c.08-.156.145-.38.18-.819.037-.45.038-1.032.038-1.889V11h2v5.241c0 .805 0 1.47-.044 2.01-.046.563-.145 1.08-.392 1.565a4 4 0 01-1.748 1.748c-.485.247-1.002.346-1.564.392-.541.044-1.206.044-2.01.044H7.758c-.805 0-1.47 0-2.01-.044-.563-.046-1.08-.145-1.565-.392a4 4 0 01-1.748-1.748c-.247-.485-.346-1.002-.392-1.564C2 17.71 2 17.046 2 16.242V11h2z"
+        fill="currentColor"
+      />
+    </svg>
+  );
+
+  return <Icon {...componentProps} component={IconNode} />;
+}
+
+UploadOutlined.displayName = 'UploadOutlined';
+export default UploadOutlined;
