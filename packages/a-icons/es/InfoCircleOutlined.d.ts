c0f10d1a05a6c920ec7c2aebce9183c4c112ef2a
LinkPeng-Spotec <link.peng@spotec.net>
2026-06-10 10:32:41 +0800

    feat: SCRUM-9734 添加信息提示的info-icon

---
+++ packages/a-icons/es/InfoCircleOutlined.d.ts
@@ -0,0 +1,6 @@
+import { IconProps } from 'a-base-icon/lib/icon';
+declare function InfoCircleOutlined(componentProps: IconProps): JSX.Element;
+declare namespace InfoCircleOutlined {
+    var displayName: string;
+}
+export default InfoCircleOutlined;
---
+++ packages/a-icons/es/InfoCircleOutlined.js
@@ -0,0 +1,31 @@
+var __assign = this && this.__assign || function () {
+  __assign = Object.assign || function (t) {
+    for (var s, i = 1, n = arguments.length; i < n; i++) {
+      s = arguments[i];
+      for (var p in s) {
+        if (Object.prototype.hasOwnProperty.call(s, p)) t[p] = s[p];
+      }
+    }
+    return t;
+  };
+  return __assign.apply(this, arguments);
+};
+import * as React from 'react';
+import Icon from 'a-base-icon/lib/icon';
+function InfoCircleOutlined(componentProps) {
+  var IconNode = function IconNode(props) {
+    return /*#__PURE__*/React.createElement("svg", __assign({
+      viewBox: "0 0 24 24",
+      fill: "none",
+      xmlns: "http://www.w3.org/2000/svg"
+    }, props), /*#__PURE__*/React.createElement("path", {
+      d: "M12 1c6.075 0 11 4.925 11 11s-4.925 11-11 11S1 18.075 1 12 5.925 1 12 1zm0 2a9 9 0 100 18 9 9 0 000-18zm1 14.5h-2v-8h2v8zm0-9h-2v-2h2v2z",
+      fill: "currentColor"
+    }));
+  };
+  return /*#__PURE__*/React.createElement(Icon, __assign({}, componentProps, {
+    component: IconNode
+  }));
+}
+InfoCircleOutlined.displayName = 'InfoCircleOutlined';
+export default InfoCircleOutlined;
