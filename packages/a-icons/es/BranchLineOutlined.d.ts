91271760beb328f8cb0b823c49e9a3a1ebeabd87
LinkPeng-Spotec <link.peng@spotec.net>
2026-06-22 11:40:41 +0800

    feat: SCRUM-9686 增加分配机会icon

---
+++ packages/a-icons/es/BranchLineOutlined.d.ts
@@ -0,0 +1,6 @@
+import { IconProps } from 'a-base-icon/lib/icon';
+declare function BranchLineOutlined(componentProps: IconProps): JSX.Element;
+declare namespace BranchLineOutlined {
+    var displayName: string;
+}
+export default BranchLineOutlined;
