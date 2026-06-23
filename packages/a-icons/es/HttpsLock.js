e1afd2412362cc7221881ab5343cc5269df71f2b
TerrinTian-Spotec <terrin.tian@spotec.net>
2026-06-23 15:57:09 +0800

    feat: SCRUM-10004 跟单：信号头像后“私人”或“公开”的标识icon更改 (#408)

--- packages/a-icons/es/HttpsLock.js
+++ packages/a-icons/es/HttpsLock.js
@@ -15,27 +15,26 @@ import Icon from 'a-base-icon/lib/icon';
 function HttpsLock(componentProps) {
   var IconNode = function IconNode(props) {
     return /*#__PURE__*/React.createElement("svg", __assign({
-      viewBox: "0 0 20 20",
+      viewBox: "0 0 24 24",
       fill: "none",
       xmlns: "http://www.w3.org/2000/svg"
     }, props), /*#__PURE__*/React.createElement("rect", {
-      x: 0.556,
-      y: 0.556,
-      width: 18.889,
-      height: 18.889,
-      rx: 9.444,
-      fill: "#EFB041"
+      x: 0.5,
+      y: 0.5,
+      width: 23,
+      height: 23,
+      rx: 11.5,
+      fill: "#FAE8E8"
     }), /*#__PURE__*/React.createElement("rect", {
-      x: 0.556,
-      y: 0.556,
-      width: 18.889,
-      height: 18.889,
-      rx: 9.444,
-      stroke: "#fff",
-      strokeWidth: 1.111
+      x: 0.5,
+      y: 0.5,
+      width: 23,
+      height: 23,
+      rx: 11.5,
+      stroke: "#fff"
     }), /*#__PURE__*/React.createElement("path", {
-      d: "M13 7.5v1.75h1.25V15h-8.5V9.25H7V7.5a3 3 0 016 0zm-5 0v1.75h4V7.5a2 2 0 10-4 0zm2 3.625a1 1 0 100 2 1 1 0 000-2z",
-      fill: "#fff"
+      d: "M15.6 9v2.1h1.5V18H6.9v-6.9h1.5V9a3.6 3.6 0 017.2 0zm-6 0v2.1h4.8V9a2.4 2.4 0 10-4.8 0zm2.4 4.35a1.2 1.2 0 100 2.4 1.2 1.2 0 000-2.4z",
+      fill: "#E06B67"
     }));
   };
   return /*#__PURE__*/React.createElement(Icon, __assign({}, componentProps, {
