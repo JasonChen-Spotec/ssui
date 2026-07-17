ab722813cd1dac74d81800d49df45837e01a6ee6
ZackZeng-Spotec <zack.zeng@spotec.net>
2026-07-17 17:14:45 +0800

    feat: SCRUM-10740 个人中心：新增“法律文件”功能

---
+++ packages/a-icons/es/LegalDocument.d.ts
@@ -0,0 +1,6 @@
+import { IconProps } from 'a-base-icon/lib/icon';
+declare function LegalDocument(componentProps: IconProps): JSX.Element;
+declare namespace LegalDocument {
+    var displayName: string;
+}
+export default LegalDocument;
