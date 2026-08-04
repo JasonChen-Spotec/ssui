a477b7c77b3db8e99a5ad22e0d842d719529e753
RavenLee <raven.lee@spotec.net>
2026-08-04 10:27:10 +0800

    feat:SCRUM-10497 全局筛选排序组件：排序&筛选样式修改(新增pc端h5列表筛选图标)

---
+++ packages/a-icons/es/FilterFileOutlined.d.ts
@@ -0,0 +1,6 @@
+import { IconProps } from 'a-base-icon/lib/icon';
+declare function FilterFileOutlined(componentProps: IconProps): JSX.Element;
+declare namespace FilterFileOutlined {
+    var displayName: string;
+}
+export default FilterFileOutlined;
---
+++ packages/a-icons/src/FilterFileOutLined.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import Icon, { IconProps } from 'a-base-icon/lib/icon';
+
+function FilterFileOutlined(componentProps: IconProps) {
+  const IconNode = (props: React.SVGProps<SVGSVGElement>) => (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" {...props}>
+      <path d="M16 17v2H8v-2h8zm3-6v2H5v-2h14zm3-6v2H2V5h20z" fill="currentColor" />
+    </svg>
+  );
+
+  return <Icon {...componentProps} component={IconNode} />;
+}
+
+FilterFileOutlined.displayName = 'FilterFileOutlined';
+export default FilterFileOutlined;
