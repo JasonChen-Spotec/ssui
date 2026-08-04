6d773834e5c1a8698be310d0dc73c122b769930a
RavenLee-Spotec <raven.lee@spotec.net>
2026-08-04 10:37:24 +0800

    feat:SCRUM-10497 全局筛选排序组件：排序&筛选样式修改(新增pc端h5列表筛选图标) (#415)

---
+++ packages/a-icons/es/FilterFileOutlined.js
@@ -0,0 +1,31 @@
+var __assign = this && this.__assign || function () {
+  __assign = Object.assign || function (t) {
+    for (var s, i = 1, n = arguments.length; i < n; i++) {
+      s = arguments[i];
+      for (var p in s) {
+        if (Object.prototype.hasOwnProperty.call(s, p)) t[p] = s[p];
+      }
+    }
+    return t;
+  };
+  return __assign.apply(this, arguments);
+};
+import * as React from 'react';
+import Icon from 'a-base-icon/lib/icon';
+function FilterFileOutlined(componentProps) {
+  var IconNode = function IconNode(props) {
+    return /*#__PURE__*/React.createElement("svg", __assign({
+      xmlns: "http://www.w3.org/2000/svg",
+      viewBox: "0 0 24 24",
+      fill: "none"
+    }, props), /*#__PURE__*/React.createElement("path", {
+      d: "M16 17v2H8v-2h8zm3-6v2H5v-2h14zm3-6v2H2V5h20z",
+      fill: "currentColor"
+    }));
+  };
+  return /*#__PURE__*/React.createElement(Icon, __assign({}, componentProps, {
+    component: IconNode
+  }));
+}
+FilterFileOutlined.displayName = 'FilterFileOutlined';
+export default FilterFileOutlined;
