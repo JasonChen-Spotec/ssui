289b529be9bc28cea80a4fd498e5bae745e6356b
LinkPeng-Spotec <link.peng@spotec.net>
2026-09-22 13:51:28 +0800

    feat: SCRUM-1251 客户详情-基本信息：统计重计算弹窗，控制台时间选择器警告

--- packages/assui/lib/label-range-picker/index.d.ts
+++ packages/assui/lib/label-range-picker/index.d.ts
@@ -3,6 +3,9 @@ import type { RangePickerProps } from 'antd/lib/date-picker';
 export interface LabelRangePickerProps extends Omit<RangePickerProps, 'label'> {
     label: React.ReactNode;
     showTime?: any;
+    popupClassName?: string;
+    /** @deprecated `dropdownClassName` 已废弃，请使用 `popupClassName` 代替。 */
+    dropdownClassName?: string;
 }
 declare const LabelDatePicker: React.FC<LabelRangePickerProps>;
 export default LabelDatePicker;
